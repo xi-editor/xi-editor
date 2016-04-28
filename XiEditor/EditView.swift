b174a8f0805fa07f30af8fbe8b1b83a9747716d9
Rob Tsuk <rob@tsuk.com>
2016-04-28 09:33:18 -0700

    Pass action commands to core
    Unless the editor view implements an action command selector, convert selector name to a command by converting camel case to underscored and removing the colon and pass this command directly to the core.

--- XiEditor/EditView.swift
+++ XiEditor/EditView.swift
@@ -69,6 +69,23 @@ func colorFromArgb(argb: UInt32) -> NSColor {
         alpha: CGFloat((argb >> 24) & 0xff) * 1.0/255)
 }
 
+func camelCaseToUnderscored(name: NSString) -> NSString {
+    let underscored = NSMutableString();
+    let scanner = NSScanner(string: name as String);
+    let notUpperCase = NSCharacterSet.uppercaseLetterCharacterSet().invertedSet;
+    var notUpperCaseFragment: NSString?
+    while (scanner.scanUpToCharactersFromSet(NSCharacterSet.uppercaseLetterCharacterSet(), intoString: &notUpperCaseFragment)) {
+        underscored.appendString(notUpperCaseFragment! as String);
+        var upperCaseFragement: NSString?
+        if (scanner.scanUpToCharactersFromSet(notUpperCase, intoString: &upperCaseFragement)) {
+            underscored.appendString("_");
+            let downcasedFragment = upperCaseFragement!.lowercaseString;
+            underscored.appendString(downcasedFragment);
+        }
+    }
+    return underscored;
+}
+
 class EditView: NSView {
 
     var lines: [[AnyObject]] = []
