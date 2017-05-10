0aeb532b042b92ec1b96b56a04acf380790cb619
Colin Rofls <colin@cmyr.net>
2017-05-10 12:11:44 -0400

    Add basic PluginActivation type

--- rust/core-lib/src/plugins/manifest.rs
+++ rust/core-lib/src/plugins/manifest.rs
@@ -25,11 +25,14 @@ use serde_json::Value;
 
 use xi_rpc::RpcLoop;
 
+use syntax::SyntaxDefinition;
 use super::PluginManager;
 use super::{Plugin, PluginRef};
 
 // example plugins. Eventually these should be loaded from disk.
 pub fn debug_plugins() -> Vec<PluginDescription> {
+    use self::PluginActivation::*;
+    use self::SyntaxDefinition::*;
     let mut path_base = env::current_exe().unwrap();
     path_base.pop();
     let make_path = |p: &str| -> PathBuf {
@@ -39,10 +42,14 @@ pub fn debug_plugins() -> Vec<PluginDescription> {
     };
 
     vec![
-        PluginDescription::new("syntect", "0.0", make_path("xi-syntect-plugin")),
-        PluginDescription::new("braces", "0.0", make_path("bracket_example.py")),
-        PluginDescription::new("spellcheck", "0.0", make_path("spellcheck.py")),
-        PluginDescription::new("shouty", "0.0", make_path("shouty.py")),
+        PluginDescription::new("syntect", "0.0", make_path("xi-syntect-plugin"),
+        vec![Autorun]),
+        PluginDescription::new("braces", "0.0", make_path("bracket_example.py"),
+        Vec::new()),
+        PluginDescription::new("spellcheck", "0.0", make_path("spellcheck.py"),
+        vec![OnSyntax(Markdown), OnSyntax(Plaintext)]),
+        PluginDescription::new("shouty", "0.0", make_path("shouty.py"),
+        vec![OnSyntax(Markdown), OnSyntax(Plaintext)])
     ]
 }
 
@@ -57,16 +64,31 @@ pub struct PluginDescription {
     // more metadata ...
     /// path to plugin executable
     pub exec_path: PathBuf,
+    /// Events that cause this plugin to run
+    activations: Vec<PluginActivation>,
+}
+
+/// `PluginActivation`s represent events that trigger running a plugin.
+#[derive(Debug, Clone)]
+pub enum PluginActivation {
+    /// Always run this plugin, when available.
+    Autorun,
+    /// Run this plugin if the provided SyntaxDefinition is active.
+    OnSyntax(SyntaxDefinition),
+    /// Run this plugin in response to a given command.
+    #[allow(dead_code)]
+    OnCommand,
 }
 
 impl PluginDescription {
-    fn new<S, P>(name: S, version: S, exec_path: P) -> Self
+    fn new<S, P>(name: S, version: S, exec_path: P, activations: Vec<PluginActivation>) -> Self
         where S: Into<String>, P: Into<PathBuf>
     {
         PluginDescription {
             name: name.into(),
             version: version.into(),
-            exec_path: exec_path.into()
+            exec_path: exec_path.into(),
+            activations: activations,
         }
     }
 
