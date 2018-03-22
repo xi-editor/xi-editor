c18cf62fff92ccbdd1d3beac531a5fb9c6a7407a
Colin Rofls <colin@cmyr.net>
2018-03-22 13:43:31 -0400

    Implement global plugin dispatch
    Also includes some additional work on the overall model. Very much a
    checkpoint.

--- rust/plugin-lib/src/global/view.rs
+++ rust/plugin-lib/src/global/view.rs
@@ -1,20 +1,37 @@
-use std::path::{Path, PathBuf};
-use serde_json::Value;
+// Copyright 2018 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-use xi_core::{ViewIdentifier, PluginPid, BufferConfig, ConfigTable, plugin_rpc};
-use xi_rpc::RpcPeer;
-use xi_rope::rope::{RopeDelta, LinesMetric};
+use std::path::{Path, PathBuf};
+use serde_json::{self, Value};
+use serde::Deserialize;
+
+use xi_core::{ViewIdentifier, PluginPid, BufferConfig, ConfigTable};
+use xi_core::plugin_rpc::{TextUnit, GetDataResponse, ScopeSpan, PluginBufferInfo};
+
+use xi_rpc::{RpcPeer, RemoteError};
+use xi_rope::rope::RopeDelta;
 
 use plugin_base::{Error, DataSource};
 
 pub struct View<C> {
-    cache: C,
+    pub (crate) cache: C,
     peer: RpcPeer,
-    path: Option<PathBuf>,
+    pub (crate) path: Option<PathBuf>,
     config: BufferConfig,
     config_table: ConfigTable,
     plugin_id: PluginPid,
-    view_id: ViewIdentifier,
+    pub (crate) view_id: ViewIdentifier,
 }
 
 struct FetchCtx {
@@ -24,12 +41,32 @@ struct FetchCtx {
 }
 
 impl<C: Cache> View<C> {
-    fn get_path(&self) -> Option<&Path> {
-        unimplemented!()
+    pub (crate) fn new(peer: RpcPeer, plugin_id: PluginPid,
+                       info: PluginBufferInfo) -> Self {
+        let PluginBufferInfo {
+            views, rev, path, config, buf_size, nb_lines, ..
+        } = info;
+
+        assert_eq!(views.len(), 1, "assuming single view");
+        let view_id = views.first().unwrap().to_owned();
+        let path = path.map(PathBuf::from);
+        View {
+            cache: C::new(buf_size, rev, nb_lines),
+            peer: peer,
+            config_table: config.clone(),
+            config: serde_json::from_value(Value::Object(config)).unwrap(),
+            path: path,
+            plugin_id: plugin_id,
+            view_id: view_id,
+        }
     }
 
-    fn get_config(&self) -> &BufferConfig {
-        unimplemented!()
+    pub fn get_path(&self) -> Option<&PathBuf> {
+        self.path.as_ref()
+    }
+
+    pub fn get_config(&self) -> &BufferConfig {
+        &self.config
     }
 
     pub fn add_scopes(&self, scopes: &Vec<Vec<String>>) {
@@ -37,7 +74,7 @@ impl<C: Cache> View<C> {
     }
 
     pub fn update_spans(&self, start: usize, len: usize,
-                        spans: &[plugin_rpc::ScopeSpan]) {
+                        spans: &[ScopeSpan]) {
 
     }
 
@@ -58,10 +95,11 @@ impl<C: Cache> View<C> {
 
 /// A cache of a document's contents
 pub trait Cache {
-    fn new(buf_size: usize, rev: u64) -> Self;
+    fn new(buf_size: usize, rev: u64, num_lines: usize) -> Self;
     fn get_line<DS>(&self, source: &DS, line_num: usize) -> Result<&str, Error>;
     /// Updates the cache by applying this delta'.
-    fn update(&mut self, delta: &RopeDelta, buf_size: usize, rev: u64);
+    fn update(&mut self, delta: Option<&RopeDelta>, buf_size: usize,
+              num_lines: usize, rev: u64);
     /// Flushes any state held by this cache.
     fn clear(&mut self);
 }
@@ -69,50 +107,31 @@ pub trait Cache {
 pub trait Plugin {
     type Cache: Cache;
 
-    fn initialize(&mut self) {
-
-    }
-
-    fn update(&mut self, view: &View<Self::Cache>, delta: RopeDelta, rev: usize) {
-
-    }
-
-    fn did_save(&mut self, view: &View<Self::Cache>) {
-
-    }
-
-    fn did_close(&mut self, view: &View<Self::Cache>) {
-
-    }
-
-    fn new_view(&mut self, view: &View<Self::Cache>) {
-
-    }
+    fn update(&mut self, view: &mut View<Self::Cache>, delta: Option<&RopeDelta>)
+        -> Result<Value, RemoteError>;
+    fn did_save(&mut self, view: &mut View<Self::Cache>, new_path: &Path);
+    fn did_close(&self, view: &View<Self::Cache>);
+    fn new_view(&mut self, view: &mut View<Self::Cache>);
 
     /// `view.config` contains the pre-change config
-    fn config_changed(&mut self, view: &View<Self::Cache>, changes: &ConfigTable) {
-
-    }
-
-    fn idle(&mut self) {
-
-    }
+    fn config_changed(&mut self, view: &mut View<Self::Cache>, changes: &ConfigTable);
+    fn idle(&mut self) { }
 }
 
 impl DataSource for FetchCtx {
-    fn get_data(&self, offset: usize, max_size: usize, rev: u64) -> Result<String, Error> {
+    fn get_data(&self, start: usize, unit: TextUnit, max_size: usize, rev: u64)
+        -> Result<GetDataResponse, Error> {
         let params = json!({
             "plugin_id": self.plugin_id,
             "view_id": self.view_id,
-            "offset": offset,
+            "start": start,
+            "unit": unit,
             "max_size": max_size,
             "rev": rev,
         });
-        let result = self.peer.send_rpc_request("get_data", &params);
-        match result {
-            Ok(Value::String(s)) => Ok(s),
-            Ok(_) => Err(Error::WrongReturnType),
-            Err(err) => Err(Error::RpcError(err)),
-        }
+        let result = self.peer.send_rpc_request("get_data", &params)
+            .map_err(|e| Error::RpcError(e))?;
+        GetDataResponse::deserialize(result)
+            .map_err(|_| Error::WrongReturnType)
     }
 }
