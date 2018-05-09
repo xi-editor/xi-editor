198b68ce6eb83698739d6585bca0ed5da83de021
Anna Scholtz <anna@scholtzan.net>
2018-05-09 16:17:29 -0700

    Simplified version to determine all search occurrences

--- rust/core-lib/src/event_context.rs
+++ rust/core-lib/src/event_context.rs
@@ -147,6 +147,7 @@ impl<'a> EventContext<'a> {
     pub(crate) fn do_plugin_cmd(&mut self, plugin: PluginId,
                                  cmd: PluginNotification) {
         use self::PluginNotification::*;
+
         match cmd {
             AddScopes { scopes } => {
                 let mut ed = self.editor.borrow_mut();
@@ -337,7 +338,7 @@ impl<'a> EventContext<'a> {
         self.with_editor(|ed, view, _| {
             let new_len = text.len();
             view.collapse_selections(ed.get_buffer());
-            view.unset_find(ed.get_buffer());
+            view.unset_find();
             let prev_sel = view.sel_regions().first().map(|s| s.clone());
             ed.reload(text);
             if let Some(prev_sel) = prev_sel {
