de7f14cc386d6975c2bf2d30e34333216ec393bf
Colin Rofls <colin@cmyr.net>
2018-05-19 11:43:32 -0400

    Sample plugin sends edits

--- rust/sample-plugin/src/main.rs
+++ rust/sample-plugin/src/main.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! A sample plugin, intended as an illustartion and a template for plugin
+//! A sample plugin, intended as an illustration and a template for plugin
 //! developers.
 
 extern crate xi_plugin_lib;
@@ -22,13 +22,20 @@ extern crate xi_rope;
 use std::path::Path;
 
 use xi_core::ConfigTable;
-use xi_core::plugin_rpc::PluginEdit;
 use xi_rope::rope::RopeDelta;
-use xi_plugin_lib::{Plugin, ChunkCache, View, mainloop};
+use xi_rope::interval::Interval;
+use xi_rope::delta::Builder as EditBuilder;
+use xi_plugin_lib::{Plugin, ChunkCache, View, mainloop, Error};
 
+/// A type that implements the `Plugin` trait, and interacts with xi-core.
+///
+/// Currently, this plugin has a single noteworthy behaviour,
+/// intended to demonstrate how to edit a document; when the plugin is active,
+/// and the user inserts an exclamation mark, the plugin will capitalize the
+/// preceding word.
 struct SamplePlugin;
 
-// implementing the `Plugin` trait is the sole requirement of a plugin.
+//NOTE: implementing the `Plugin` trait is the sole requirement of a plugin.
 // For more documentation, see `rust/plugin-lib` in this repo.
 impl Plugin for SamplePlugin {
     type Cache = ChunkCache;
@@ -48,10 +55,55 @@ impl Plugin for SamplePlugin {
     fn config_changed(&mut self, _view: &mut View<Self::Cache>, _changes: &ConfigTable) {
     }
 
-    fn update(&mut self, view: &mut View<Self::Cache>, _delta: Option<&RopeDelta>,
-              _edit_type: String, _author: String) -> Option<PluginEdit> {
-        eprintln!("edit in view {}", view.get_id());
-        None
+    fn update(&mut self, view: &mut View<Self::Cache>, delta: Option<&RopeDelta>,
+              _edit_type: String, _author: String) {
+
+        //NOTE: example simple conditional edit. If this delta is
+        //an insert of a single '!', we capitalize the preceding word.
+
+        if let Some(delta) = delta {
+            let (iv, _) = delta.summary();
+            let text: String = delta.as_simple_insert()
+                .map(String::from)
+                .unwrap_or_default();
+            if text == "!" {
+                let _ = self.capitalize_word(view, iv.end());
+            }
+        }
+    }
+}
+
+impl SamplePlugin {
+    /// Uppercases the word preceding `end_offset`.
+    fn capitalize_word(&self, view: &mut View<ChunkCache>, end_offset: usize)
+        -> Result<(), Error>
+    {
+        //NOTE: this makes it clear to me that we need a better API for edits
+        let line_nb = view.line_of_offset(end_offset)?;
+        let line_start = view.offset_of_line(line_nb)?;
+
+        let mut cur_utf8_ix = 0;
+        let mut word_start = 0;
+        for c in view.get_line(line_nb)?.chars() {
+            if c.is_whitespace() {
+                word_start = cur_utf8_ix;
+            }
+
+            cur_utf8_ix += c.len_utf8();
+
+            if line_start + cur_utf8_ix == end_offset {
+                break;
+            }
+        }
+
+        let new_text = view.get_line(line_nb)?[word_start..end_offset-line_start]
+            .to_uppercase();
+        let buf_size = view.get_buf_size();
+        let mut builder = EditBuilder::new(buf_size);
+        let iv = Interval::new_closed_open(line_start + word_start, end_offset);
+        builder.replace(iv, new_text.into());
+        view.edit(builder.build(), 0, false, true, "sample".into());
+        Ok(())
     }
 }
 
