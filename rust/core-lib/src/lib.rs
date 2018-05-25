9406b46a0186b3df49230a7b234da560c94330cd
Colin Rofls <colin@cmyr.net>
2018-05-25 12:54:43 -0400

    Simplify default config handling
    This is a refactor in response to language configs no longer being bundled
    with core. Eventually we would like the config system to have no
    knowledge of file paths at all.

--- rust/core-lib/src/lib.rs
+++ rust/core-lib/src/lib.rs
@@ -36,8 +36,6 @@ extern crate serde;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
-#[macro_use]
-extern crate lazy_static;
 extern crate time;
 extern crate syntect;
 extern crate toml;
