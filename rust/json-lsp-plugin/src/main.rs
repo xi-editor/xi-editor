7da85be302eee46ab2171f55fc8a7439660d5a3a
betterclever <paliwal.pranjal83@gmail.com>
2018-06-07 02:33:50 +0530

    Added Workspace based LSP Plugin Support

--- rust/json-lsp-plugin/src/main.rs
+++
@@ -1,7 +0,0 @@
-extern crate xi_lsp_lib;
-use xi_lsp_lib::{start_mainloop, LSPPlugin};
-
-fn main() {
-    let mut plugin = LSPPlugin::new("vscode-json-languageserver", &["--stdio"]);
-    start_mainloop(&mut plugin);
-}
