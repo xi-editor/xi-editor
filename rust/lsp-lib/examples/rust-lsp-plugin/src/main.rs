989e3c48797eedcf1f64731634bbc2f53a8b64fd
betterclever <paliwal.pranjal83@gmail.com>
2018-06-07 22:26:40 +0530

    Added License headers in files

--- rust/lsp-lib/examples/rust-lsp-plugin/src/main.rs
+++ rust/lsp-lib/examples/rust-lsp-plugin/src/main.rs
@@ -1,7 +1,22 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 extern crate xi_lsp_lib;
 use xi_lsp_lib::{start_mainloop, LSPPlugin};
 
 fn main() {
+    
     // Assuming RLS is in default path i.e. ~/.cargo/bin/rls
     // TODO: Make this configurable
     let home_dir = std::env::home_dir();
