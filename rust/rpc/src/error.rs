b2d09ba1e6f75dc6209c858c8ff87ea2b605a242
Chojan Shang <psiace@outlook.com>
2020-01-04 03:51:55 +0800

    Allow needless doctest main.

--- rust/rpc/src/error.rs
+++ rust/rpc/src/error.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #![allow(clippy::needless_doctest_main)]
 
 use std::fmt;
