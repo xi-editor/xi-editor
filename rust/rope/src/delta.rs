7dc525ce0edcdfb39440ca2f2fa415473997341c
Tobias Decking <Tobias.Decking@gmail.com>
2020-06-26 23:50:11 +0200

    Fix panic in is_simple_delete (#1280)
    * fix panic in is_simple_delete
    
    * docstring
    
    * rustfmt

--- rust/rope/src/delta.rs
+++ rust/rope/src/delta.rs
@@ -89,10 +89,13 @@ impl<N: NodeInfo> Delta<N> {
     }
 
     /// Returns `true` if this delta represents a single deletion without
-    /// any insertions. Note that this is `false` for the trivial delta.
+    /// any insertions.
+    ///
+    /// Note that this is `false` for the trivial delta, as well as for a deletion
+    /// from an empty `Rope`.
     pub fn is_simple_delete(&self) -> bool {
-        if self.els.is_empty() && self.base_len > 0 {
-            return true;
+        if self.els.is_empty() {
+            return self.base_len > 0;
         }
         if let DeltaElement::Copy(beg, end) = self.els[0] {
             if beg == 0 {
@@ -827,6 +830,9 @@ mod tests {
     #[test]
     fn is_simple_delete() {
         let d = Delta::simple_edit(10..12, Rope::from("+"), TEST_STR.len());
+        assert_eq!(false, d.is_simple_delete());
+
+        let d = Delta::simple_edit(Interval::new(0, 0), Rope::from(""), 0);
         assert_eq!(false, d.is_simple_delete());
 
         let d = Delta::simple_edit(Interval::new(10, 11), Rope::from(""), TEST_STR.len());
